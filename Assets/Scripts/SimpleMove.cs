b296dc176494cd695576095fe4ad1d55f10cc348
Jan Marek Kunert <jan.kunert@r132-32-w10.SCHULE-ISERV>
2018-04-05 14:36:45 +0200

    0.5

--- Assets/Scripts/SimpleMove.cs
+++ Assets/Scripts/SimpleMove.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimpleMove : MonoBehaviour
 {
     public float speed = 5.0f;
     public float sprintingSpeed = 3.0f;
     private bool sprinting=false;
+    private bool sneaking = false;
     private float rotationChangingSpeed = 10000.0f;
     public float rotationSpeed = 1.0f;
     private Vector3 moveDirection = Vector3.zero;
     private Quaternion destRotate;
     //private Vector3 destRotation;
-
+    private bool pause;
     private CharacterController controller;
     //private Quaternion bordermax;
     //private Quaternion bordermin;
@@ -20,10 +22,17 @@ public class SimpleMove : MonoBehaviour
     public float steplength = 0.4f;
     public float delay = 0;
     public float volume = 0.7f;
+    public Text pauseText;
+    public GameObject MainCamera;
+    public GameObject gameController;
+
 
     // Use this for initialization
     void Start()
     {
+        
+        pauseText.enabled = false;
+        Cursor.visible = false;
         controller = GetComponent<CharacterController>();
         destRotate = transform.rotation;
         //bordermax = Quaternion.Euler(80,0,0);
@@ -37,67 +46,116 @@ public class SimpleMove : MonoBehaviour
         //Quaternion actRotation = transform.rotation;
         //destRotation.x = actRotation.x + Input.GetAxis("MY") * rotationSpeed;
         //Vector3 view = destRotation;
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        if (sprinting)
+        if (pause == false)
         {
-            moveDirection = moveDirection * (speed + sprintingSpeed);
-        }
-        else
-        {
-            moveDirection = moveDirection * speed;
-        }
-
-        moveDirection = transform.TransformDirection(moveDirection);
-        controller.SimpleMove(moveDirection);
-        destRotate.eulerAngles -= new Vector3(Input.GetAxis("MY") * rotationSpeed, 0, 0);
-        //destRotate.SetLookRotation(view);
-        destRotate.eulerAngles += new Vector3(0, Input.GetAxis("MouseX") * rotationSpeed, 0);
-
-
-        /*if (Input.GetKey(KeyCode.Q))
-        {
-            destRotate.eulerAngles -= new Vector3(0, rotationSpeed, 0);
-
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            destRotate.eulerAngles += new Vector3(0, rotationSpeed, 0);
-
-
-        }*/
-
-        
-
-
-        float step = rotationChangingSpeed * Time.deltaTime;
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, destRotate, step);
-
-
-        if (controller.velocity.sqrMagnitude > 0.2f)
-        {
-            if (delay > steplength)
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            if (sprinting)
             {
-                AudioSource.PlayClipAtPoint(aud, transform.position, volume);
-                
-                if(sprinting==false)delay = 0;
-                if (sprinting) delay = 0.1f;
+                moveDirection = moveDirection * (speed + sprintingSpeed);
+            }
+            else
+            {
+                moveDirection = moveDirection * speed;
+            }
+
+            moveDirection = transform.TransformDirection(moveDirection);
+            controller.SimpleMove(moveDirection);
+            destRotate.eulerAngles -= new Vector3(Input.GetAxis("MY") * rotationSpeed, 0, 0);
+            //destRotate.SetLookRotation(view);
+            destRotate.eulerAngles += new Vector3(0, Input.GetAxis("MouseX") * rotationSpeed, 0);
+
+
+            /*if (Input.GetKey(KeyCode.Q))
+            {
+                destRotate.eulerAngles -= new Vector3(0, rotationSpeed, 0);
 
             }
+            if (Input.GetKey(KeyCode.E))
+            {
+                destRotate.eulerAngles += new Vector3(0, rotationSpeed, 0);
+
+
+            }*/
+
+
+
+
+            float step = rotationChangingSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, destRotate, step);
+
+
+            if (controller.velocity.sqrMagnitude > 0.2f)
+            {
+                if (delay > steplength)
+                {
+                    AudioSource.PlayClipAtPoint(aud, transform.position, volume);
+
+                    if (sprinting == false) delay = 0;
+                    if (sprinting) delay = 0.1f;
+
+                }
+            }
+            delay += Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Cursor.visible = true;
+                pauseText.enabled = true;
+                pause = true;
+                Time.timeScale = 0.0f;
+                //gameController.GetComponent<AudioSource>.pause
+            }
+            
         }
-        delay += Time.deltaTime;
+        else if (pause == true && Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            Cursor.visible = false;
+            pause = false;
+            pauseText.enabled = false;
+            Time.timeScale = 1.0f;
+
+        }
+
+
     }
     private void Sprint()
     {
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (pause == false)
         {
-            sprinting = true;
-            AudioSource.PlayClipAtPoint(aud, transform.position, volume);
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift)) {
 
-               sprinting = false;
-            delay = 0;
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                sprinting = true;
+                MainCamera.transform.position += new Vector3(0,0.1F,0);
+                AudioSource.PlayClipAtPoint(aud, transform.position, volume);
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+
+                sprinting = false;
+                MainCamera.transform.position += new Vector3(0, -0.1F, 0);
+                delay = 0;
+            }
+        }
+    }
+    private void Sneak()
+    {
+        if (pause == false)
+        {
+
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                sneaking = true;
+                MainCamera.transform.position += new Vector3(0, -0.1F, 0);
+                AudioSource.PlayClipAtPoint(aud, transform.position, volume);
+            }
+            if (Input.GetKeyUp(KeyCode.LeftControl))
+            {
+
+                sneaking = false;
+                MainCamera.transform.position += new Vector3(0, +0.1F, 0);
+                delay = 0;
+            }
         }
     }
 
